f8783d6534bdcded6124e8ec12bf16a5bf1d706f
Aviad Cohen <aviadcoh1@gmail.com>
2020-09-22 15:07:36 +0300

    deployTry3

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser')
 const api = require('./server/routes/api')
 const app = express()
 const mongoose = require('mongoose')
+require('dotenv').config()
 const port = process.env.PORT || 8080
 
 mongoose.set('useFindAndModify', false)
 // mongoose.connect("mongodb://localhost/WeatherApp")
-mongoose.connect(process.env.MONGO_URI)
+
+mongoose.connect(`${process.env.MONGO_URI}`)
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
@@ -16,8 +18,4 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', api)
 
-app.get('/', function(req, res){
-  res.redirect('/todo');
-});
-
-app.listen(port, () => console.log("Server up and running on port" + port))
+app.listen(port, () => console.log("Server up and running on port " + port))
